711b470c559c94afa181b87745efeceaa4666eaa
Rahul Kavi <rahulkavi@gmail.com>
2013-05-01 23:34:59 -0400

    final version 1a

---
+++ build/CMakeFiles/LR.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason/Desktop/LogisticRegression-cpp/src/LogisticRegression.cpp" "/home/jason/Desktop/LogisticRegression-cpp/build/CMakeFiles/LR.dir/src/LogisticRegression.cpp.o"
+  "/home/jason/Desktop/LogisticRegression-cpp/src/Main.cpp" "/home/jason/Desktop/LogisticRegression-cpp/build/CMakeFiles/LR.dir/src/Main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/LR.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LR.dir/src/Main.cpp.o"
+  "CMakeFiles/LR.dir/src/LogisticRegression.cpp.o"
+  "LR.pdb"
+  "LR"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
